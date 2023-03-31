4bb9cc54df7b720b7bab4272f2534be34218172f
nazmunnaharnijhum <nazmunnaharnijhum@gmail.com>
2023-03-31 10:34:07 +0600

    components added

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,12 +4,18 @@ import './Shop.css';
 
 const Shop = () => {
     const [products,setProducts] = useState([]);
+    const [cart,setCart] = useState([])
 
     useEffect(() => {
 fetch('products.json')
 .then(res => res.json())
 .then(data => setProducts(data));
-    }, []);
+    }, []); 
+     const handleAddToCart = (product) => {
+        // cart.push(product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
     return (
         <div className='shop-container'>
           <div className="products-container">
@@ -17,11 +23,13 @@ fetch('products.json')
                 products.map(product => <Product
                 key={product.id}
                 product = {product}
+                handleAddToCart={handleAddToCart}
                 ></Product>)
             }
             </div>  
             <div className="cart-container">
                 <h4>Order summary</h4>
+                <p>Selected Items: {cart.length}</p>
             </div>
         </div>
     );
